b3228c196b20557b5ab782e21125aff055b8b10c
Ismingiz <email@domain.com>
2024-11-12 23:02:08 -1200

    first commit

---
+++ main.sql
@@ -0,0 +1,51 @@
+-- 1-masala
+-- INSERT INTO foydalanuvchilar (ism, yosh, manzil)
+-- VALUES
+-- ('Muhammadjon', 17, 'Fargona, Toshloq'),
+-- ('Temurbek', 34, 'Toshkent, olmaliq'),
+-- ('Rustam', 22, 'Samarqand, Yakkabog');
+
+-- SELECT * FROM foydalanuvchilar;
+
+
+-- -- 2-masala
+-- INSERT INTO mahsulotlar (nom, narx, tavsif)
+-- VALUES
+-- ('macBook', 10000, 'silver rangli songi model'),
+-- ('Telefon', 540, '1 tb xotira'),
+-- ('Kompyuter', 900, 'acer, ryzen5, black, 1tb xotira');
+
+--SELECT * FROM mahsulotlar;
+
+
+-- -- 3-masala
+-- INSERT INTO buyurtmalar (id, foydalanuvchi, mahsulot, miqdor)
+-- VALUES
+-- (1, 'Muhammadjon', 'kitob, butsa, laptop, shingalka, futa', 5),
+-- (2, 'Muhammadali', 'kapa, shlem', 2),
+-- (3, 'Ramzon', 'UFC belt', 10);
+
+-- SELECT * FROM buyurtmalar;
+
+
+-- -- 4-masala
+-- INSERT INTO xodimlar (id, ism, lavozim, yillik_ish_haqi)
+-- VALUES
+-- (1, 'Davronbek', 'python dasturchisi', 15000000),
+-- (2, 'Alisher', 'c++ dasturchisi', 20000000),
+-- (3, 'Muhammadjon', 'python dasturchisi', 18000000);
+
+-- SELECT * FROM xodimlar;
+
+
+-- -- 5-masala
+-- INSERT INTO yetkazib_beruvchilar (id, kompaniya_nomi, telefon_raqam, manzil)
+-- VALUES
+-- (1, 'AbuTech', '998941139977', 'Fargona, sayilgoh kochasi'),
+-- (2, 'BTS', '998912345678', 'fargona, toshloq tumani');
+
+-- SELECT * FROM yetkazib_beruvchilar;
+
+
+
+
